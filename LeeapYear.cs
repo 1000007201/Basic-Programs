62930e278b8d467fc337981c94310750b36ee9b4
1000007201 <nishant.sharma5504@gmail.com>
2022-01-30 15:23:19 +0530

    [Nishant] Add:Leap Year

---
+++ LeeapYear.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    public class LeeapYear
+    {
+        public void YearCheck()
+        {
+            Console.WriteLine("Enter Year to be checked:");
+            int year = (int)Convert.ToInt32(Console.ReadLine());
+            if (Convert.ToString(year).Length == 4)
+            {
+                if (year % 4 == 0)
+                {
+                    Console.WriteLine("{0} Year is a Leap Year", year);
+                }
+                else
+                {
+                    Console.WriteLine("{0} Year is not Leap Year", year);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please Enter valid Year");
+            }
+        }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,7 +5,10 @@ bool flag = true;
 
 while (flag)
 {
-    Console.WriteLine("Enter Value:\n1.Head and Tail percentage\n2.Exit");
+    Console.WriteLine("Enter Value:\n" +
+        "1.Head and Tail percentage\n" +
+        "2.Check Leap Year\n"+
+        "3.Exit");
     int value = Convert.ToInt32(Console.ReadLine());
     switch (value)
     {
@@ -14,6 +17,10 @@ while (flag)
             toss.TossResult();
             break;
         case 2:
+            LeeapYear leapyear = new LeeapYear();
+            leapyear.YearCheck();
+            break;
+        case 3:
             flag = false;
             break;
     }
