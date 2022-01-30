ef521ac8f0df6455f43539d483ceb1bfa6670552
1000007201 <nishant.sharma5504@gmail.com>
2022-01-30 15:13:33 +0530

    [Nishant] Add:Head and Tail Program

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BasicProgram;
+bool flag = true;
+
+
+while (flag)
+{
+    Console.WriteLine("Enter Value:\n1.Head and Tail percentage\n2.Exit");
+    int value = Convert.ToInt32(Console.ReadLine());
+    switch (value)
+    {
+        case 1:
+            Toss toss = new Toss();
+            toss.TossResult();
+            break;
+        case 2:
+            flag = false;
+            break;
+    }
+
+}
---
+++ Toss.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProgram
+{
+    public class Toss
+    {
+        int head_count = 0;
+        int tail_count = 0;
+        public void TossResult()
+        {
+            Random rand = new Random();
+            Console.WriteLine("Enter number of times coin flipped:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                double random_number = rand.NextDouble();
+                if (random_number < 0.5)
+                {
+                    tail_count++;
+                }
+                else
+                {
+                    head_count++;
+                }
+
+            }
+            Console.WriteLine("Percentage of Head: {0}\nPercentage of Tail: {1}", ((double)head_count / (double)number) * 100, ((double)tail_count / (double)number) * 100);
+
+        }
+
+    }
+}
